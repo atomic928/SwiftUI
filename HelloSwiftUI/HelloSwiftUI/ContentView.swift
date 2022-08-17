58aff78fdb6e562df733342926bf65f49eb1867c
deguchi daimu <deguchi@enPiT2020MBP-04.local>
2022-08-17 16:35:38 +0900

    ボタンの追加

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, SwiftUI!")
+                .padding()
+            Button("ボタン") {
+                print("ボタンが押されたよ")
+            }
+        }
     }
 }
 
