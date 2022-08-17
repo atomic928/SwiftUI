209b2707775fe455c77048fe26ec4c7fc9555566
deguchi daimu <deguchi@enPiT2020MBP-04.local>
2022-08-17 17:34:17 +0900

    VStack, HStack, ZStackの練習

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -8,15 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var str = "Hello, SwiftUI!"
     var body: some View {
         VStack {
-            Text(str)
-                .padding()
-                .foregroundColor(.red)
-            Button("ボタン") {
-                str = "ハローSwiftUI!"
-                print("ボタンが押されたよ")
+            Rectangle()
+                .foregroundColor(.gray)
+                .frame(width: 350, height: 100)
+            Rectangle()
+                .foregroundColor(.purple)
+                .frame(width: 350, height: 100)
+            HStack {
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(width: 150, height: 150)
+                Rectangle()
+                    .foregroundColor(.red)
+                    .frame(width: 100, height: 100)
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 50, height: 50)
+            }
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.green)
+                    .frame(width: 350, height: 200)
+                HStack {
+                    VStack {
+                        Rectangle()
+                            .foregroundColor(.pink)
+                            .frame(width: 150, height: 70)
+                        Rectangle()
+                            .foregroundColor(.black)
+                            .frame(width: 150, height: 70)
+                    }
+                    VStack {
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .frame(width: 150, height: 70)
+                        Rectangle()
+                            .foregroundColor(.yellow)
+                            .frame(width: 150, height: 70)
+                    }
+                }
             }
         }
     }
