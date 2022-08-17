d3d235c9c994732e21a423ce8ce5b82fe2846c99
atomic928 <b1020055@fun.ac.jp>
2022-08-17 20:51:02 +0900

    add tutorial

---
+++ tutorial/tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tutorial
+//
+//  Created by deguchi daimu  on 2022/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tutorial/tutorial/tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorialApp.swift
+//  tutorial
+//
+//  Created by deguchi daimu  on 2022/08/17.
+//
+
+import SwiftUI
+
+@main
+struct tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
