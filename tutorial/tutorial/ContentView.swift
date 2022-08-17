b66147d6ead10bd5f723ab30997d8b90d18b0319
atomic928 <b1020055@fun.ac.jp>
2022-08-17 21:53:42 +0900

    map

--- tutorial/tutorial/ContentView.swift
+++ tutorial/tutorial/ContentView.swift
@@ -6,27 +6,16 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868), span: MKCoordinateSpan(
+            latitudeDelta: 0.2, longitudeDelta: 0.2
+        )
+    )
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Tutle Fock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park.")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
-            }
-            Image("turtlerock")
-                .clipShape(Circle())
-                .overlay {
-                    Circle().stroke(.white, lineWidth: 4)
-                }
-                .shadow(radius: 7)
-        }
-        .padding()
+        Map(coordinateRegion: $region)
     }
 }
 
