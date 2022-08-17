845710eaec214cc1b58bf191a862524340ab162c
deguchi daimu <deguchi@enPiT2020MBP-04.local>
2022-08-17 16:39:24 +0900

    文字色の変更、ボタンを押すと文字が変わるようにした

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var str = "Hello, SwiftUI!"
     var body: some View {
         VStack {
-            Text("Hello, SwiftUI!")
+            Text(str)
                 .padding()
+                .foregroundColor(.red)
             Button("ボタン") {
+                str = "ハローSwiftUI!"
                 print("ボタンが押されたよ")
             }
         }
