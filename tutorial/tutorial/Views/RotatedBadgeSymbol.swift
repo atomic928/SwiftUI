bbeac5b1864d41c782193aa30b825bf31ee0aff5
atomic928 <b1020055@fun.ac.jp>
2022-08-19 16:21:56 +0900

    badgeのforeground作成

---
+++ tutorial/tutorial/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  tutorial
+//
+//  Created by deguchi daimu  on 2022/08/19.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
